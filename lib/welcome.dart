09e049c46093cfd02c57bc379b5aa7b721fb81c8
Scott Semtner <sjsemtner@gmail.com>
2020-06-30 23:49:43 -0700

    working on drawing wave thing. broken right now

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,38 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:madeease_app/waves.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SvgPicture.asset(
-              'assets/logos/m.svg',
-              height: 200,
-              placeholderBuilder: (BuildContext context) => Container(
-                  padding: EdgeInsets.all(30.0),
-                  child: CircularProgressIndicator()),
-            ),
-            SvgPicture.asset(
-              'assets/wave1.svg',
-              placeholderBuilder: (BuildContext context) => Container(
-                  height: 100,
-                  padding: EdgeInsets.all(30.0),
-                  child: CircularProgressIndicator()),
-            ),
-            RaisedButton(
-              onPressed: () => {},
-              color: Theme.of(context).primaryColor,
-              child: Text("Get Started"),
-            )
-          ],
-        ),
-      ),
-    );
+    // return Scaffold(
+    //   body: SizedBox(
+    //     width: MediaQuery.of(context).size.width,
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    //       crossAxisAlignment: CrossAxisAlignment.center,
+    //       children: <Widget>[
+    //         SvgPicture.asset(
+    //           'assets/logos/m.svg',
+    //           height: 200,
+    //           placeholderBuilder: (BuildContext context) => Container(
+    //               padding: EdgeInsets.all(30.0),
+    //               child: CircularProgressIndicator()),
+    //         ),
+    //         Wave(),
+    //         RaisedButton(
+    //           onPressed: () => {},
+    //           color: Theme.of(context).primaryColor,
+    //           child: Text("Get Started"),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
+    return SizedBox(width: 500, height: 500, child: Wave());
   }
 }
